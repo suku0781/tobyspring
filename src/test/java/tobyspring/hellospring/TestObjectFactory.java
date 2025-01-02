96f8daeae5ca7e0dfc927b25b10c7f2d2e5cafc5
suku0781 <suku0781@gmail.com>
2025-01-02 21:29:21 +0900

    테스트와 DI 수업 2 완강 후 커밋(자동 di(spring DI ) 를 사용한 di 테스트 )

---
+++ src/test/java/tobyspring/hellospring/TestObjectFactory.java
@@ -0,0 +1,25 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.payment.ExRateProviderStub;
+import tobyspring.hellospring.payment.PaymentService;
+
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.valueOf;
+
+@Configuration
+public class TestObjectFactory {
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new ExRateProviderStub(valueOf(1_000));
+    }
+}
---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -0,0 +1,41 @@
+package tobyspring.hellospring.payment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.hellospring.ObjectFactory;
+import tobyspring.hellospring.TestObjectFactory;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.valueOf;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
+class PaymentServiceSpringTest {
+
+    @Autowired PaymentService paymentService;
+    @Autowired ExRateProviderStub exRateProviderStub;
+    @Test
+    @DisplayName("prepare 메소드가 요구사항 3개지를 잘 충족했는지 검증")// 테스트 성공 또는 실패 시 어떤 테스트가 실행되었는지 뜨는 JUnit 기능
+    void prepare() throws IOException {
+        // exRate : 1000
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
+
+        // exRate : 5000
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment2.getExRate()).isEqualByComparingTo(valueOf(500));
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
+    }
+}
