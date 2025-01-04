aa47d0251311b946ee8719f235afaff01f47c8ae
suku0781 <suku0781@gmail.com>
2025-01-04 20:49:08 +0900

    ApiTemplate 분리 (람다식)

---
+++ src/main/java/tobyspring/hellospring/api/ApiTemplate.java
@@ -0,0 +1,44 @@
+package tobyspring.hellospring.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ApiTemplate {
+    /*
+     * 메서드로 구분해놓은 runApiForExRate (템플릿이라 부름) 잘변하지 않는 속성을 가진 코드를 넣어놓음.
+     * 변하는 속성을 가진 코드는 콜백 형태로 만들어서 템플릿에 메서드 파라미터 형태로 전달함.
+     * 콜백이 있고, 템플릿이 있고, 이 템플릿의 메서드를 호출해 주는 코드가 getExRate인데 이것을 클라이언트라고 부름.
+     *
+     * 클라이언트가 콜백을 만들어서 템플릿을 호출함.
+     * 클라이언트가 콜백을 직접 만들면서 템플릿의 메서드를 호출한다.
+     *
+     * 클라이언트, 콜백, 템플릿 세가지가 협력해서 일을 하는 구조가 됨.
+     * */
+    public static BigDecimal getExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+        String response;
+        URI uri;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        try{
+            response = apiExecutor.excute(uri);
+
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+        try {
+            return exRateExtractor.extract(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,29 +1,35 @@
 package tobyspring.hellospring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import tobyspring.hellospring.api.ApiExecutor;
-import tobyspring.hellospring.api.ErApiExRateExtractor;
-import tobyspring.hellospring.api.ExRateExtractor;
-import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.api.*;
 import tobyspring.hellospring.payment.ExRateProvider;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 
 public class WebApiExRateProvider  implements ExRateProvider {
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
+
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
+        return apiTemplate.getExRate(url, uri -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .build();
+
+
+            try(HttpClient client = HttpClient.newBuilder().build()){
+                return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, new ErApiExRateExtractor());
 
         /*
         * 람다식으로 콜백을 만들어서 던지면 굳이 클래스를 만들지 않아도 됨.
@@ -32,38 +38,6 @@ public class WebApiExRateProvider  implements ExRateProvider {
         * */
     }
 
-    /*
-    * 메서드로 구분해놓은 runApiForExRate (템플릿이라 부름) 잘변하지 않는 속성을 가진 코드를 넣어놓음.
-    * 변하는 속성을 가진 코드는 콜백 형태로 만들어서 템플릿에 메서드 파라미터 형태로 전달함.
-    * 콜백이 있고, 템플릿이 있고, 이 템플릿의 메서드를 호출해 주는 코드가 getExRate인데 이것을 클라이언트라고 부름.
-    *
-    * 클라이언트가 콜백을 만들어서 템플릿을 호출함.
-    * 클라이언트가 콜백을 직접 만들면서 템플릿의 메서드를 호출한다.
-    *
-    * 클라이언트, 콜백, 템플릿 세가지가 협력해서 일을 하는 구조가 됨.
-    * */
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        String response;
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        try{
-            response = apiExecutor.excute(uri);
-
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 
 }
