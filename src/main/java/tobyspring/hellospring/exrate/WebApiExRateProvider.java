2722f9005c3f6beb65534d846ea6560709917739
suku0781 <suku0781@gmail.com>
2025-01-04 18:44:22 +0900

    변경 가능성이 있는 코드 부분을 메서드로 추출하는 작업 수행

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -18,9 +18,10 @@ import java.util.stream.Collectors;
 public class WebApiExRateProvider  implements ExRateProvider {
     @Override
     public BigDecimal getExRate(String currency) {
-        URI uri;
-        String url = "https://open.er-api.com/v6/latest/" + currency;
+        String url = "https://open.er-api.com/v6/latest/" + currency; // 변경 가능성 없는 코드
+        // 변경 가능성 있는 코드 start
         String response;
+        URI uri;
         try {
             uri = new URI(url);
         } catch (URISyntaxException e) {
@@ -28,31 +29,45 @@ public class WebApiExRateProvider  implements ExRateProvider {
         }
 
         try{
-            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            response = excuteApi(uri);
 
-            try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                response = br.lines().collect(Collectors.joining());
-            }
-            /*
-            * try, catch가 실행하고 close를 보장하기 위해서
-            * 자바에서 try-with-resource로 finally 블록을 사용하지 않고도
-            * 블록안에서 어떤 일이 일어나건 관계없이 끝나면 리소스를 close()해줄 수 있는 기능을 제공함.
-            *
-            * try안에 변수 선언부를 모두 ()에 집어넣고 실행되는 코드를 {}안에 넣어주면 close()를 선언하지 않고도
-            * try() 에 선언해 놓은 것을 블록 안에서 처리되고 내려가든 예외가 발생해서 exception이 던져지든 관계없이
-            * close()를 해줌.
-            * */
         } catch (IOException e) {
             throw new RuntimeException();
         }
 
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            ExRateData data = mapper.readValue(response, ExRateData.class);
-
-            return data.rates().get("KRW");
+            return parseExRate(response);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+        // 변경 가능성 있는 코드 end
+    }
+
+    private static BigDecimal parseExRate(String response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+
+        return data.rates().get("KRW");
+    }
+
+    private static String excuteApi(URI uri) throws IOException {
+        String response;
+        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            response = br.lines().collect(Collectors.joining());
+        }
+
+        /*
+         * try, catch가 실행하고 close를 보장하기 위해서
+         * 자바에서 try-with-resource로 finally 블록을 사용하지 않고도
+         * 블록안에서 어떤 일이 일어나건 관계없이 끝나면 리소스를 close()해줄 수 있는 기능을 제공함.
+         *
+         * try안에 변수 선언부를 모두 ()에 집어넣고 실행되는 코드를 {}안에 넣어주면 close()를 선언하지 않고도
+         * try() 에 선언해 놓은 것을 블록 안에서 처리되고 내려가든 예외가 발생해서 exception이 던져지든 관계없이
+         * close()를 해줌.
+         * */
+        
+        return response;
     }
 }
