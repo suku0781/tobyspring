f6d7b9f6acdd4777eced1474c972cecce7d752c5
suku0781 <suku0781@gmail.com>
2025-01-07 00:09:40 +0900

    DataAccessException과 예외 추상화(예외템플릿을 활용한 예외 추상화작업)

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -3,8 +3,14 @@ package tobyspring.hellospring;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import org.aspectj.weaver.ast.Or;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 import tobyspring.hellospring.payment.PaymentService;
@@ -15,15 +21,25 @@ public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        // transaction begin
+        try {
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-        System.out.println(order);
-        Order order2 = new Order("100", BigDecimal.ONE);
-        repository.save(order2);
+                System.out.println(order);
 
+                Order order2 = new Order("100", BigDecimal.ONE);
+                repository.save(order2);
+                return null;
+            });
+        } catch (DataIntegrityViolationException e){
+            System.out.println("주문 번호 중복 복구 작업");
 
+        }
+        // commit
 
     }
 }
