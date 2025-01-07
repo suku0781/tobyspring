f9a451343e3a538c4311dc2704dfbd93eadeee5e
suku0781 <suku0781@gmail.com>
2025-01-07 21:44:35 +0900

    트랜잭션을 관리하는 오브젝트를 추상화 하는 작업

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -2,6 +2,7 @@ package tobyspring.hellospring.order;
 
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.JpaOrderRepository;
 
@@ -11,17 +12,17 @@ import java.math.BigDecimal;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final JpaTransactionManager jpaTransactionManager;
+    private final PlatformTransactionManager transactionManager; // JPA를 쓰는 트랜잭션 메니저이든 JDBC기술을 사용하는 트랜잭션 매니저이든 관계없이 코드는 그대로 변경되지않고 유지됨.
 
-    public OrderService(OrderRepository orderRepository, JpaTransactionManager jpaTransactionManager) {
+    public OrderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
         this.orderRepository = orderRepository;
-        this.jpaTransactionManager = jpaTransactionManager;
+        this.transactionManager = transactionManager;
     }
 
     public Order createOrder(String no, BigDecimal total) {
         Order order = new Order(no, total);
 
-        return new TransactionTemplate(jpaTransactionManager).execute(status -> {
+        return new TransactionTemplate(transactionManager).execute(status -> {
             this.orderRepository.save(order);
             return order;
         });
