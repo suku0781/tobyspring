78c10fc4a86917e22e61afd428e574c0b471cead
suku0781 <suku0781@gmail.com>
2025-01-07 20:56:49 +0900

    Order.java 에 있는 jpa 기술을 제거하고 META-INF디렉터리안에 orm.xml 파일에 JPA를 대신하여 사용할 수 있도록 정보를 입력하여 기술종속성을 제거하는 작업 수행

--- src/main/java/tobyspring/hellospring/OrderClient.java
+++ src/main/java/tobyspring/hellospring/OrderClient.java
@@ -15,11 +15,9 @@ public class OrderClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(OrderConfig.class);
         OrderService service = beanFactory.getBean(OrderService.class);
-        // JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
-
-
 
         Order order = service.createOrder("0100", BigDecimal.TEN);
         System.out.println(order);
+
     }
 }
